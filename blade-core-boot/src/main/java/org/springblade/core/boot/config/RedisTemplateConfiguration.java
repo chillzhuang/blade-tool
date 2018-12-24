9b6b1e657ca7c46ad90c502323273ba6f899b8f0
smallchill <smallchill@163.com>
2018-12-24 13:53:45 +0800

    :zap: 增加redis配置

---
+++ blade-core-boot/src/main/java/org/springblade/core/boot/config/RedisTemplateConfiguration.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2018-2028, Chill Zhuang 庄骞 (smallchill@163.com).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springblade.core.boot.config;
+
+import org.springblade.core.boot.redis.RedisKeySerializer;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * RedisTemplate 配置
+ */
+@EnableCaching
+@Configuration
+@AutoConfigureBefore(RedisAutoConfiguration.class)
+public class RedisTemplateConfiguration {
+
+	/**
+	 * value 值 序列化
+	 * @return RedisSerializer
+	 */
+	@Bean
+	@ConditionalOnMissingBean(RedisSerializer.class)
+	public RedisSerializer<Object> redisSerializer() {
+		return new JdkSerializationRedisSerializer();
+	}
+
+	@Bean(name = "redisTemplate")
+	@ConditionalOnMissingBean(RedisTemplate.class)
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory, RedisSerializer<Object> redisSerializer) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		RedisKeySerializer redisKeySerializer = new RedisKeySerializer();
+		// key 序列化
+		redisTemplate.setKeySerializer(redisKeySerializer);
+		redisTemplate.setHashKeySerializer(redisKeySerializer);
+		// value 序列化
+		redisTemplate.setValueSerializer(redisSerializer);
+		redisTemplate.setHashValueSerializer(redisSerializer);
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		return redisTemplate;
+	}
+
+	@Bean
+	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.entryTtl(Duration.ofHours(1)); // 设置缓存有效期一小时
+		return RedisCacheManager
+			.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+			.cacheDefaults(redisCacheConfiguration).build();
+	}
+
+}
