d1b750069e14014d833bd646a76a1037276024f8
smallchill <smallchill@163.com>
2018-12-29 18:46:01 +0800

    :zap: 更新license说明

--- blade-core-swagger/src/main/java/org/springblade/core/swagger/SwaggerAutoConfiguration.java
+++ blade-core-swagger/src/main/java/org/springblade/core/swagger/SwaggerAutoConfiguration.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2018-2028, lengleng (wangiegie@gmail.com).
  * <p>
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE;
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
