b6866d4691a78a16d8089f82b90b0aeba8f7cd6e
smallchill <smallchill@163.com>
2018-12-29 18:40:16 +0800

    :zap: 优化代码

--- blade-core-mybatis/src/main/java/org/springblade/core/mp/base/BaseEntity.java
+++ blade-core-mybatis/src/main/java/org/springblade/core/mp/base/BaseEntity.java
@@ -18,7 +18,6 @@ package org.springblade.core.mp.base;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -79,7 +78,6 @@ public class BaseEntity implements Serializable {
 	/**
 	 * 状态[0:未删除,1:删除]
 	 */
-	@TableLogic
 	@ApiModelProperty(value = "是否已删除")
 	private Integer isDeleted;
 }
