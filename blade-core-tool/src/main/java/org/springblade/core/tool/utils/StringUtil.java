a26bbc36caab48274c039f4babdba94741594699
smallchill <smallchill@163.com>
2019-02-24 13:41:12 +0800

    :tada: 2.0.7.RELEASE

--- blade-core-tool/src/main/java/org/springblade/core/tool/utils/StringUtil.java
+++ blade-core-tool/src/main/java/org/springblade/core/tool/utils/StringUtil.java
@@ -1365,6 +1365,7 @@ public class StringUtil extends org.springframework.util.StringUtils {
 	 * @return String
 	 */
 	public static String humpToUnderline(String para) {
+		para = lowerFirst(para);
 		StringBuilder sb = new StringBuilder(para);
 		int temp = 0;
 		for (int i = 0; i < para.length(); i++) {
@@ -1376,6 +1377,44 @@ public class StringUtil extends org.springframework.util.StringUtils {
 		return sb.toString().toLowerCase();
 	}
 
+	/**
+	 * 横线转驼峰
+	 *
+	 * @param para 字符串
+	 * @return String
+	 */
+	public static String lineToHump(String para) {
+		StringBuilder result = new StringBuilder();
+		String[] a = para.split("-");
+		for (String s : a) {
+			if (result.length() == 0) {
+				result.append(s.toLowerCase());
+			} else {
+				result.append(s.substring(0, 1).toUpperCase());
+				result.append(s.substring(1).toLowerCase());
+			}
+		}
+		return result.toString();
+	}
+
+	/**
+	 * 驼峰转横线
+	 *
+	 * @param para 字符串
+	 * @return String
+	 */
+	public static String humpToLine(String para) {
+		para = lowerFirst(para);
+		StringBuilder sb = new StringBuilder(para);
+		int temp = 0;
+		for (int i = 0; i < para.length(); i++) {
+			if (Character.isUpperCase(para.charAt(i))) {
+				sb.insert(i + temp, "-");
+				temp += 1;
+			}
+		}
+		return sb.toString().toLowerCase();
+	}
 
 }
 
