55b092665b65d489ad92513923577eeae782bb01
如梦技术 <596392912@qq.com>
2024-03-09 15:45:00 +0800

    :sparkles: 完善工具类

--- blade-core-tool/src/main/java/org/springblade/core/tool/utils/CharPool.java
+++ blade-core-tool/src/main/java/org/springblade/core/tool/utils/CharPool.java
@@ -21,16 +21,51 @@ package org.springblade.core.tool.utils;
  * @author L.cm
  */
 public interface CharPool {
-
-	char UPPER_A		= 'A';
-	char LOWER_A		= 'a';
-	char UPPER_Z		= 'Z';
-	char LOWER_Z		= 'z';
-	char DOT			= '.';
-	char AT				= '@';
-	char LEFT_BRACE		= '{';
-	char RIGHT_BRACE	= '}';
-	char LEFT_BRACKET	= '(';
-	char RIGHT_BRACKET	= ')';
-
+	// @formatter:off
+	char UPPER_A          = 'A';
+	char LOWER_A          = 'a';
+	char UPPER_Z          = 'Z';
+	char LOWER_Z          = 'z';
+	char DOT              = '.';
+	char AT               = '@';
+	char LEFT_BRACE       = '{';
+	char RIGHT_BRACE      = '}';
+	char LEFT_BRACKET     = '(';
+	char RIGHT_BRACKET    = ')';
+	char DASH             = '-';
+	char PERCENT          = '%';
+	char PIPE             = '|';
+	char PLUS             = '+';
+	char QUESTION_MARK    = '?';
+	char EXCLAMATION_MARK = '!';
+	char EQUALS           = '=';
+	char AMPERSAND        = '&';
+	char ASTERISK         = '*';
+	char STAR             = ASTERISK;
+	char BACK_SLASH       = '\\';
+	char COLON            = ':';
+	char COMMA            = ',';
+	char DOLLAR           = '$';
+	char SLASH            = '/';
+	char HASH             = '#';
+	char HAT              = '^';
+	char LEFT_CHEV        = '<';
+	char NEWLINE          = '\n';
+	char N                = 'n';
+	char Y                = 'y';
+	char QUOTE            = '\"';
+	char RETURN           = '\r';
+	char TAB              = '\t';
+	char RIGHT_CHEV       = '>';
+	char SEMICOLON        = ';';
+	char SINGLE_QUOTE     = '\'';
+	char BACKTICK         = '`';
+	char SPACE            = ' ';
+	char TILDA            = '~';
+	char LEFT_SQ_BRACKET  = '[';
+	char RIGHT_SQ_BRACKET = ']';
+	char UNDERSCORE       = '_';
+	char ONE              = '1';
+	char ZERO             = '0';
+	// @formatter:on
 }
