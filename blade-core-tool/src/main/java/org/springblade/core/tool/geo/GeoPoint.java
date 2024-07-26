ca85cf1077c424eb3a2bc89998ea65e53de5a7af
如梦技术 <596392912@qq.com>
2024-07-26 12:48:56 +0800

    :sparkles: 条件 geo 坐标转换

---
+++ blade-core-tool/src/main/java/org/springblade/core/tool/geo/GeoPoint.java
@@ -0,0 +1,43 @@
+package org.springblade.core.tool.geo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 坐标点
+ *
+ * @author JourWon、hutool、L.cm
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeoPoint implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 3584864663880053897L;
+
+	/**
+	 * 经度
+	 */
+	private double lon;
+	/**
+	 * 纬度
+	 */
+	private double lat;
+
+	/**
+	 * 当前坐标偏移指定坐标
+	 *
+	 * @param offset 偏移量
+	 * @return this
+	 */
+	public GeoPoint offset(GeoPoint offset) {
+		this.lon += offset.lon;
+		this.lat += offset.lat;
+		return this;
+	}
+
+}
