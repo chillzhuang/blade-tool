2980a709688d002f5084d99da49c42715c6cfbdc
如梦技术 <596392912@qq.com>
2025-01-02 12:40:43 +0800

    :sparkles: 调整 sql 日志打印

--- blade-starter-mybatis/src/main/java/org/springblade/core/mp/config/BladeMybatisPlusProperties.java
+++ blade-starter-mybatis/src/main/java/org/springblade/core/mp/config/BladeMybatisPlusProperties.java
@@ -20,9 +20,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * druid 连接池配置
  *
